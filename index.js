dd903ce48a47f3d8cb239d94f5566962f5878893
Assault1892 <vvvvvgaga12356@yahoo.co.jp>
2025-03-06 14:04:36 +0900

    おんがくエンドポイントのじゅんび

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const ongaku = require("./o.json");
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
